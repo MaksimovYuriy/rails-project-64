35d7cdb2ef17c837c60bc6dbf8a64b368b53b5cd
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-02-20 17:06:56 +0300

    Переход на slim

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,4 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def index
-    render plain: 'Hello, World!'
-    begin
-      1 / 0
-    rescue ZeroDivisionError => exception
-      Sentry.capture_exception(exception)
-    end
-    
-    Sentry.capture_message("test message")
-  end
-
 end
