58b0a1c6a712adc12cd2ab671fa5dcefed6f467b
YuraMaksimov <yura.maksimov.04@mail.ru>
2025-03-07 13:37:08 +0300

    Контроллеры devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :users
   resources :posts do
     resources :post_comments, controller: 'comments', only: [:create], as: :comments
